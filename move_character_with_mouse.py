9aa8f2729ddd811d94c81174c6eb2d2ba1827a0a
Tamach4n <jinhyung119@naver.com>
2024-10-03 16:17:15 +0900

    클릭 시 손 출력

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -4,16 +4,22 @@ TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
+hand = load_image('hand_arrow.png')
 character = load_image('animation_sheet.png')
 
+def randomHand():
+    global mx, my
 
 def handle_events():
     global running
     global x, y
+    global mx, my
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            mx, my = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
@@ -24,6 +30,7 @@ def handle_events():
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+mx, my = 0, 0
 frame = 0
 hide_cursor()
 
@@ -31,6 +38,7 @@ while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.draw(mx, my)
     update_canvas()
     frame = (frame + 1) % 8
 
